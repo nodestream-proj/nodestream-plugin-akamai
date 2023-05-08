cc3e91600f8d5dbd744926febfff32b60824dd01
ccloes <ccloes@gmail.com>
2023-05-07 19:15:34 -0700

    Updated to add siteshield and fix gtm and waf loaders

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import logging
 import os
 
-#import akamai_property_cacher, akamai_redirect_cacher, akamai_gtm_cacher, akamai_netstorage_cacher, akamai_appsec_cacher, akamai_siteshield_cacher, akamai_waf_cacher, akamai_ehn_cacher, akamai_cps_cacher, neo4j_property_loader, neo4j_redirect_loader, neo4j_gtm_loader, neo4j_netstorage_loader, neo4j_appsec_loader, neo4j_siteshield_loader, neo4j_waf_loader, neo4j_ehn_loader, neo4j_cps_loader
-import akamai_property_cacher, akamai_redirect_cacher, akamai_gtm_cacher, akamai_netstorage_cacher, akamai_appsec_cacher, akamai_siteshield_cacher, akamai_waf_cacher, akamai_ehn_cacher, akamai_cps_cacher, neo4j_property_loader, neo4j_redirect_loader, neo4j_gtm_loader, neo4j_netstorage_loader, neo4j_appsec_loader, neo4j_waf_loader, neo4j_ehn_loader, neo4j_cps_loader
+import akamai_property_cacher, akamai_redirect_cacher, akamai_gtm_cacher, akamai_netstorage_cacher, akamai_appsec_cacher, akamai_siteshield_cacher, akamai_waf_cacher, akamai_ehn_cacher, akamai_cps_cacher, neo4j_property_loader, neo4j_redirect_loader, neo4j_gtm_loader, neo4j_netstorage_loader, neo4j_appsec_loader, neo4j_siteshield_loader, neo4j_waf_loader, neo4j_ehn_loader, neo4j_cps_loader
 
 SELECTED_PIPELINES = os.environ["SELECTED_PIPELINES"].split(",")
 PIPELINE_FACTORIES = {
@@ -20,7 +19,7 @@ PIPELINE_FACTORIES = {
     "neo4j_gtm_loader": neo4j_gtm_loader.make_pipeline,
     "neo4j_netstorage_loader": neo4j_netstorage_loader.make_pipeline,
     "neo4j_appsec_loader": neo4j_appsec_loader.make_pipeline,
-#    "neo4j_siteshield_loader": neo4j_siteshield_loader.make_pipeline,
+    "neo4j_siteshield_loader": neo4j_siteshield_loader.make_pipeline,
     "neo4j_waf_loader": neo4j_waf_loader.make_pipeline,
     "neo4j_ehn_loader": neo4j_ehn_loader.make_pipeline,
     "neo4j_cps_loader": neo4j_cps_loader.make_pipeline,
