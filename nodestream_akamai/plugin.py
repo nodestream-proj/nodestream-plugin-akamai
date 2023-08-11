1e7e3e142fef96b4efcbfee71e77eebd22f936bb
grantleehoffman <grantleehoffman@gmail.com>
2023-08-11 10:49:01 -0700

    update plugin definition

--- nodestream_akamai/plugin.py
+++ nodestream_akamai/plugin.py
@@ -1,26 +1,17 @@
-from nodestream.project import Project, ProjectPlugin, PipelineScope, PipelineDefinition
+from nodestream.project import Project, ProjectPlugin, PipelineScope
 
 class AkamaiPlugin(ProjectPlugin):
     def activate(self, project: Project) -> None:
         scopes = [
-            PipelineScope.from_resources(name="appsec-coverage-cacher", package="nodestream_akamai.appsec_coverage"),
-            PipelineScope.from_resources(name="appsec-coverage-loader", package="nodestream_akamai.appsec_coverage"),
-            PipelineScope.from_resources(name="cps-cacher", package="nodestream_akamai.cps"),
-            PipelineScope.from_resources(name="cps-loader", package="nodestream_akamai.cps"),
-            PipelineScope.from_resources(name="ehn-cacher", package="nodestream_akamai.ehn"),
-            PipelineScope.from_resources(name="ehn-loader", package="nodestream_akamai.ehn"),
-            PipelineScope.from_resources(name="gtm-cacher", package="nodestream_akamai.gtm"),
-            PipelineScope.from_resources(name="gtm-loader", package="nodestream_akamai.gtm"),
-            PipelineScope.from_resources(name="netstorage-cacher", package="nodestream_akamai.netstorage"),
-            PipelineScope.from_resources(name="netstorage-loader", package="nodestream_akamai.netstorage"),
-            PipelineScope.from_resources(name="propery-cacher", package="nodestream_akamai.property"),
-            PipelineScope.from_resources(name="property-loader", package="nodestream_akamai.property"),
-            PipelineScope.from_resources(name="redirect-cacher", package="nodestream_akamai.redirect"),
-            PipelineScope.from_resources(name="redirect-loader", package="nodestream_akamai.redirect"),
-            PipelineScope.from_resources(name="siteshield-cacher", package="nodestream_akamai.siteshield"),
-            PipelineScope.from_resources(name="siteshield-loader", package="nodestream_akamai.siteshield"),
-            PipelineScope.from_resources(name="waf-cacher", package="nodestream_akamai.waf"),
-            PipelineScope.from_resources(name="waf-loader", package="nodestream_akamai.waf"),
+            PipelineScope.from_resources(name="appsec-coverage", package="nodestream_akamai.appsec_coverage"),
+            PipelineScope.from_resources(name="cps", package="nodestream_akamai.cps"),
+            PipelineScope.from_resources(name="ehn", package="nodestream_akamai.ehn"),
+            PipelineScope.from_resources(name="gtm", package="nodestream_akamai.gtm"),
+            PipelineScope.from_resources(name="netstorage", package="nodestream_akamai.netstorage"),
+            PipelineScope.from_resources(name="propery", package="nodestream_akamai.property"),
+            PipelineScope.from_resources(name="redirect", package="nodestream_akamai.redirect"),
+            PipelineScope.from_resources(name="siteshield", package="nodestream_akamai.siteshield"),
+            PipelineScope.from_resources(name="waf", package="nodestream_akamai.waf"),
         ]
         for scope in scopes:
             project.add_scope(scope)
