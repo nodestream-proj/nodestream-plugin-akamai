787d882e173d9e13123b4a8c41e89068c6363dff
grantleehoffman <grantleehoffman@gmail.com>
2023-08-16 08:24:25 -0700

    update packag naming, update nodestream version

--- nodestream_akamai/plugin.py
+++ nodestream_akamai/plugin.py
@@ -3,15 +3,15 @@ from nodestream.project import Project, ProjectPlugin, PipelineScope
 class AkamaiPlugin(ProjectPlugin):
     def activate(self, project: Project) -> None:
         scopes = [
-            PipelineScope.from_resources(name="appsec-coverage", package="nodestream_akamai.appsec_coverage"),
-            PipelineScope.from_resources(name="cps", package="nodestream_akamai.cps"),
-            PipelineScope.from_resources(name="ehn", package="nodestream_akamai.ehn"),
-            PipelineScope.from_resources(name="gtm", package="nodestream_akamai.gtm"),
-            PipelineScope.from_resources(name="netstorage", package="nodestream_akamai.netstorage"),
-            PipelineScope.from_resources(name="propery", package="nodestream_akamai.property"),
-            PipelineScope.from_resources(name="redirect", package="nodestream_akamai.redirect"),
-            PipelineScope.from_resources(name="siteshield", package="nodestream_akamai.siteshield"),
-            PipelineScope.from_resources(name="waf", package="nodestream_akamai.waf"),
+            PipelineScope.from_resources(name="akamai_appsec-coverage", package="nodestream_akamai.appsec_coverage"),
+            PipelineScope.from_resources(name="akamai_cps", package="nodestream_akamai.cps"),
+            PipelineScope.from_resources(name="akamai_ehn", package="nodestream_akamai.ehn"),
+            PipelineScope.from_resources(name="akamai_gtm", package="nodestream_akamai.gtm"),
+            PipelineScope.from_resources(name="akamai_netstorage", package="nodestream_akamai.netstorage"),
+            PipelineScope.from_resources(name="akamai_propery", package="nodestream_akamai.property"),
+            PipelineScope.from_resources(name="akamai_redirect", package="nodestream_akamai.redirect"),
+            PipelineScope.from_resources(name="akamai_siteshield", package="nodestream_akamai.siteshield"),
+            PipelineScope.from_resources(name="akamai_waf", package="nodestream_akamai.waf"),
         ]
         for scope in scopes:
             project.add_scope(scope)
