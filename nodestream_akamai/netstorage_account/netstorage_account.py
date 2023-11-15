167c0b54acba4222f3c70adbc14604d87cac4dca
Stuart Macleod <smacleod@akamai.com>
2023-11-15 13:52:22 -0800

    Removing redundant files, updating nestorage yaml

--- nodestream_akamai/netstorage_account/netstorage_account.py
+++ nodestream_akamai/netstorage_account/netstorage_account.py
@@ -17,4 +17,19 @@ class AkamaiNetstorageAccountExtractor(Extractor):
             self.logger.error("Failed to list netstorage upload accounts: %s", err)
 
         for account in upload_accounts:
+            account["combinedKeys"] = []
+            key_types = ["ftp", "ssh", "rsync", "aspera", "g2o"]
+            for key_type in key_types:
+                if key_type in account["keys"].keys():
+                    for key in account["keys"][key_type]:
+                        parsed_key = {
+                            "type": key_type,
+                            "identity": key["identity"],
+                            "lastModifiedBy": key["lastModifiedBy"],
+                            "lastModifiedDate": key["lastModifiedDate"],
+                        }
+                        account["combinedKeys"].append(parsed_key)
+
+            # Remove keys element before pushing to database
+            del account["keys"]
             yield account
