8464578f85a443adab672e2390dc5e62991ce44f
Stuart Macleod <heystuartmacleod@gmail.com>
2023-12-22 18:20:33 -0800

    Reordering imports

--- nodestream_akamai/__init__.py
+++ nodestream_akamai/__init__.py
@@ -1,12 +1,12 @@
+from .plugin import AkamaiPlugin
 from .appsec_coverage import AkamaiAppSecCoverageExtractor
 from .cps import AkamaiCPSExtractor
 from .ehn import AkamaiEHNExtractor
 from .gtm import AkamaiGTMExtractor
-from .iam_users import AkamaiIAMUserExtractor
 from .iam_clients import AkamaiIAMClientExtractor
+from .iam_users import AkamaiIAMUserExtractor
 from .netstorage_account import AkamaiNetstorageAccountExtractor
 from .netstorage_group import AkamaiNetstorageGroupExtractor
-from .plugin import AkamaiPlugin
 from .property import AkamaiPropertyExtractor
 from .redirect import AkamaiRedirectExtractor
 from .siteshield import AkamaiSiteShieldExtractor
@@ -18,8 +18,8 @@ __all__ = (
     "AkamaiCPSExtractor",
     "AkamaiEHNExtractor",
     "AkamaiGTMExtractor",
-    "AkamaiIAMUserExtractor",
     "AkamaiIAMClientExtractor",
+    "AkamaiIAMUserExtractor",
     "AkamaiNetstorageAccountExtractor",
     "AkamaiNetstorageGroupExtractor",
     "AkamaiPropertyExtractor",
