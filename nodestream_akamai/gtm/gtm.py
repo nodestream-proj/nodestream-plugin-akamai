11e02f74dd2eec2b970e62d8bc2aac32bb293d3f
Stuart Macleod <heystuartmacleod@gmail.com>
2024-10-30 17:02:06 -0700

    Simplifying code in line with EDNS, adding performance property type

--- nodestream_akamai/gtm/gtm.py
+++ nodestream_akamai/gtm/gtm.py
@@ -1,5 +1,5 @@
 import logging
-from ipaddress import IPv4Address, IPv6Address, ip_address
+from ipaddress import IPv4Address, ip_address
 
 from nodestream.pipeline.extractors import Extractor
 
@@ -10,73 +10,77 @@ class AkamaiGtmExtractor(Extractor):
     def __init__(self, **akamai_client_kwargs) -> None:
         self.client = AkamaiGtmClient(**akamai_client_kwargs)
         self.logger = logging.getLogger(self.__class__.__name__)
-
-    def get_target_type(self, target) -> str:
-        parsed_target = "endpoint"
-        try:
-            parsed_ip = ip_address(target)
-            if type(parsed_ip) is IPv4Address:
-                parsed_target = "ipv4"
-            elif type(parsed_ip) is IPv6Address:
-                parsed_target = "ipv6"
-        except ValueError:
-            pass
-        return parsed_target
-
-    async def extract_records(self):
         # We only extract linked ipv4, ipv6 and endpoint types from these property types
-        parsed_types = [
+        self.PARSED_PROPERTY_TYPES = [
             "weighted-round-robin",
             "weighted-hashed",
             "weighted-round-robin-load-feedback",
             "ranked-failover",
             "failover",
+            "performance",
         ]
+        self.DEEPLINK_PREFIX = "https://control.akamai.com/apps/gtm/#/domains/"
+        self.ADDRESS_FORMAT_TO_NODE_TYPE = {
+            "ipv4": "Cidripv4",
+            "ipv6": "Cidripv6",
+            "endpoint": "Endpoint",
+        }
+
+    def _get_address_format(self, address):
+        try:
+            if type(ip_address(address)) is IPv4Address:
+                return "ipv4"
+            else:
+                return "ipv6"
+        except ValueError:
+            if " " in address:
+                return "Invalid"
+            else:
+                return "endpoint"
+
+    async def extract_records(self):
         for domain in self.client.list_gtm_domains():
             try:
                 raw_domain = self.client.get_gtm_domain(domain["name"])
             except Exception as err:
                 self.logger.error(f"Failed to get gtm domain: {domain['name']}")
                 self.logger.error(err)
-            deeplink_prefix = "https://control.akamai.com/apps/gtm/#/domains/"
-            deeplink = f'{deeplink_prefix}{domain["name"]}/properties/list'
+
+            deeplink = f'{self.DEEPLINK_PREFIX}{domain["name"]}/properties/list'
             properties = []
             for property in raw_domain["properties"]:
                 fqdn = property["name"] + "." + raw_domain["name"]
-                endpoints = []
-                Cidripv4 = []
-                Cidripv6 = []
-                if property["type"] in parsed_types:
+                out_property = {
+                    "fqdn": fqdn,
+                    "type": property["type"],
+                    "name": property["name"],
+                    "Endpoint": [],
+                    "Cidripv4": [],
+                    "Cidripv6": [],
+                }
+
+                if property["type"] in self.PARSED_PROPERTY_TYPES:
                     for traffic_target in property["trafficTargets"]:
                         if len(traffic_target["servers"]) > 0:
                             for server in traffic_target["servers"]:
-                                parsed_server = self.get_target_type(server)
-                                if parsed_server == "ipv4":
-                                    Cidripv4.append(server)
-                                elif parsed_server == "ipv6":
-                                    Cidripv6.append(server)
-                                else:
-                                    endpoints.append(server)
+                                address_format = self._get_address_format(server)
+                                node_type = self.ADDRESS_FORMAT_TO_NODE_TYPE.get(
+                                    address_format
+                                )
+                                if node_type:
+                                    out_property[node_type].append(server)
                         if traffic_target["handoutCName"]:
-                            endpoints.append(traffic_target["handoutCName"])
-                for record in property["staticRRSets"]:
-                    if record["type"] == "A":
-                        Cidripv4.extend(record["rdata"])
-                    elif record["type"] == "AAAA":
-                        Cidripv6.extend(record["rdata"])
-                    elif record["type"] == "CNAME":
-                        endpoints.extend(record["rdata"])
+                            out_property["Endpoint"].append(
+                                traffic_target["handoutCName"]
+                            )
+                for rrset in property["staticRRSets"]:
+                    for record in rrset["rdata"]:
+                        address_format = self._get_address_format(record)
+                        node_type = self.ADDRESS_FORMAT_TO_NODE_TYPE.get(address_format)
+                        if node_type:
+                            out_property[node_type].append(record)
 
-                properties.append(
-                    {
-                        "fqdn": fqdn,
-                        "type": property["type"],
-                        "name": property["name"],
-                        "endpoints": endpoints,
-                        "Cidripv4": Cidripv4,
-                        "Cidripv6": Cidripv6,
-                    }
-                )
+                properties.append(out_property)
             parsed_domain = {
                 "name": raw_domain["name"],
                 "properties": properties,
