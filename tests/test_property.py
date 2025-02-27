3da083c69f8c135c3bd0167e91f0d28291f62cce
Jon Bristow <jonthegm@yahoo.com>
2025-02-27 15:00:22 -0800

    fix: unused test

--- tests/test_property.py
+++ tests/test_property.py
@@ -4,7 +4,6 @@ import pytest
 from requests import HTTPError
 
 from nodestream_akamai import AkamaiPropertyExtractor
-from nodestream_akamai.akamai_utils.property_client import NoRuleFoundError
 
 
 @pytest.fixture
@@ -24,23 +23,6 @@ async def test_extract_records_fail_list_props(extractor):
     extractor.client.list_all_properties = Mock(side_effect=HTTPError("test error"))
     with pytest.raises(HTTPError):
         _ignore = [x async for x in extractor.extract_records()]
-
-
-@pytest.mark.asyncio
-async def test_extract_records_fail_no_rule(extractor):
-    extractor.client.list_all_properties = Mock(
-        return_value=[
-            {},
-            {
-                "productionVersion": "test",
-                "propertyName": "test-name",
-                "propertyId": "1234",
-            },
-        ]
-    )
-    extractor.client.describe_property_by_dict = Mock(side_effect=NoRuleFoundError)
-
-    assert [x async for x in extractor.extract_records()] == []
 
 
 @pytest.mark.asyncio
